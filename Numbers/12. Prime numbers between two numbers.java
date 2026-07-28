925862508fcb375b6aa99b562ce94b7c64b298f9
dhanushrij44 <dhanushrij44@gmail.com>
2026-07-28 20:32:54 +0530

    Add files via upload

---
+++ Numbers/12. Prime numbers between two numbers.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+
+        for (int n = start; n <= end; n++) {
+            int count = 0;
+
+            for (int i = 1; i <= n; i++) {
+                if (n % i == 0) {
+                    count++;
+                }
+            }
+
+            if (count == 2) {
+                System.out.print(n + " ");
+            }
+        }
+    }
+}
