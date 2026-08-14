ce6a2301327b3ac20d0550047e1f99b2e3c067e8
dhanushrij44 <dhanushrij44@gmail.com>
2026-08-14 22:59:23 +0530

    Add files via upload

---
+++ Matrix/13. Diagonal sum.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[][] matrix = new int[n][n];
+
+     
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        int sum = 0;
+
+       
+        for (int i = 0; i < n; i++) {
+            sum = sum + matrix[i][i];
+        }
+
+        System.out.println(sum);
+    }
+}
