305d69768d05534e9ac6d3ce936d0412e0531a6f
dhanushrij44 <dhanushrij44@gmail.com>
2026-08-17 21:31:06 +0530

    Add files via upload

---
+++ String/13. Remove vowels.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            if (ch != 'a' && ch != 'e' && ch != 'i' &&
+                ch != 'o' && ch != 'u') {
+
+                result = result + ch;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
