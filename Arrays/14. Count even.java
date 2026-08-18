702fd52e42774c33939f6da2a63418e3f928665f
dhanushrij44 <dhanushrij44@gmail.com>
2026-08-18 22:56:14 +0530

    Add files via upload

---
+++ Arrays/14. Count even.java
@@ -0,0 +1,17 @@
+public class Counteven {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 15, 20, 25, 30};
+
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] % 2 == 0) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
