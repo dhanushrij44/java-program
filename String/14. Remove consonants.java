ecdeaaf7ea505b5bb5c7ced553dee74e51e3b4c9
dhanushrij44 <dhanushrij44@gmail.com>
2026-08-22 22:10:15 +0530

    Add files via upload

---
+++ String/14. Remove consonants.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || 
+                ch == 'o' || ch == 'u') {
+                System.out.print(ch);
+            }
+        }
+    }
+}
