5e2888000b568bb0c51ae6fc5fdf717522690e49
dhanushrij44 <dhanushrij44@gmail.com>
2026-08-29 22:56:28 +0530

    Add files via upload

---
+++ Matrix/16. Upper triangular.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] a = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Upper Triangular Matrix:");
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i <= j)
+                    System.out.print(a[i][j] + " ");
+                else
+                    System.out.print("0 ");
+            }
+            System.out.println();
+        }
+    }
+}
