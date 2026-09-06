5db5bf5e590f87b4f96c2f7aba357a0711e69f0f
dhanushrij44 <dhanushrij44@gmail.com>
2026-09-06 20:30:52 +0530

    Add files via upload

---
+++ String/17. Remove duplicates.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    public static int removeDuplicates(int[] nums) {
+        int k = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2, 2, 3};
+
+        int result = removeDuplicates(nums);
+
+        System.out.println(result);
+
+        for (int i = 0; i < result; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
