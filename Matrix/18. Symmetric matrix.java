e790cd4b091e9a5585b774828ed963145f5e0ca5
dhanushrij44 <dhanushrij44@gmail.com>
2026-09-08 17:59:35 +0530

    Add files via upload

---
+++ Matrix/18. Symmetric matrix.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[][] a = new int[n][n];
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        boolean symmetric = true;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(a[i][j] != a[j][i]) {
+                    symmetric = false;
+                }
+            }
+        }
+
+        if(symmetric)
+            System.out.println("Symmetric");
+        else
+            System.out.println("Not Symmetric");
+    }
+}
